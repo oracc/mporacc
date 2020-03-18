c086644bb5814641c46e6728623fee24dc3b4cfe
Steve Tinney <stinney@upenn.edu>
2020-03-17 21:20:25 -0400

    revisions to mp-init-common

--- mp-init-common.sh
+++ mp-init-common.sh
@@ -7,11 +7,11 @@ fi
 multipass launch --name $mpname --mem 8G --disk 120G
 multipass exec $mpname sudo apt-add-repository universe
 multipass exec $mpname sudo apt-add-repository multiverse
-multipass exec $mpname sudo apt upgrade
+multipass exec $mpname sudo apt update
 multipass exec $mpname git clone https://github.com/oracc/mporacc.git
 multipass exec $mpname git clone https://github.com/oracc/oracc.git
 multipass exec $mpname git clone https://github.com/oracc/oraccdoc.git
 multipass exec $mpname git clone https://github.com/oracc/osys.git
 multipass exec $mpname sudo oracc/pkg-ubuntu18.sh
-multipass exec $mpname oracc/perl-modules.sh
 multipass exec $mpname oracc/force-bash.sh
+#multipass exec $mpname oracc/perl-modules.sh
